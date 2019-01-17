add18b976dbf7583595a5c20d86bcc07259427d3
collincates <collin.cates@gmail.com>
2019-01-17 14:12:18 -0800

    Building out Card model

--- mtgsdkwork.py
+++ mtgsdkwork.py
@@ -1,6 +1,21 @@
 import requests
 from mtgsdk import Card
 
+# 44057
+
+grab = 44357    pages 1-444
+grab2 = 44357   pages 1-449
+grab3 = Card.all()
+
+# function/lcass to get all of the cards within the sdk
+class CardGrabber(object):
+    def __init__(self):
+        self.cards = []
+        self.get_cards()
+    def get_cards(self):
+        for i in range(1, 450):
+            self.cards.extend(Card.where(page=i).all())
+            print(f'got page {i}')
 
 class CardModelFactory(object):
     def __init__(self, mtgsdk_card):
@@ -24,45 +39,45 @@ for card, attrs in data.items():
 
 
 class Card(models.Model):
-    artist = models.
-    border = models.
-    cmc = models.
-    color_identity = models.
-    colors = models.
-    flavor = models.
-    foreign_names = models.
-    hand = models.
-    id = models.
-    image_url = models.    f'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid={multiverse_id}&type=card'
-    layout = models.
-    legalities = models.
-    life = models.
-    loyalty = models.
-    mana_cost = models.
-    multiverse_id = models.
-    name = models.
-    names = models.
-    number = models.
-    original_text = models.
-    original_type = models.
-    power = models.
-    printings = models.
-    rarity = models.
-    release_date = models.
-    rulings = models.
-    set = models.
-    set_name = models.
-    source = models.
-    starter = models.
-    subtypes = models.
-    supertypes = models.
-    text = models.
-    timeshifted = models.
-    toughness = models.
-    type = models.
-    types = models.
-    variations = models.
-    watermark = models.Model
+    artist = models.CharField(max_length=100)
+    border = models.CharField(max_length=10)
+    cmc = models.FloatField()
+    color_identity = models. """list of strings""" # arraylist postgres? serialize/deserialize json string?
+    colors = models. """list of strings""" # arraylist postgres? serialize/deserialize json string?
+    flavor = models.TextField(max_length=1000)
+    foreign_names = models. """list of dictionaries""" # arraylist postgres? serialize/deserialize json string?
+    hand = models. # Nonetype?
+    id = models.CharField(max_length=64)
+    image_url = models.URLField(max_length=200)
+    layout = models.CharField(max_length=15)
+    legalities = models. """list of dictionaries""" # arraylist postgres? serialize/deserialize json string?
+    life = models. # Nonetype?
+    loyalty = models.CharField(max_length=10)
+    mana_cost = models.CharField(max_length=100)
+    multiverse_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=255)
+    names = models. """list of strings""" # arraylist postgres? serialize/deserialize json string?
+    number = models.CharField(max_length=10)
+    original_text = models.TextField(max_length=1000)
+    original_type = models.CharField(max_length=255)
+    power = models.CharField(max_length=10)
+    printings = models. """list of strings""" # arraylist postgres? serialize/deserialize json string?
+    rarity = models.CharField(max_length=50)
+    release_date = models.DateField()
+    rulings = models. """list of dictionaries""" # arraylist postgres? serialize/deserialize json string?
+    set = models.CharField(max_length=10)
+    set_name = models.CharField(max_length=255)
+    source = models.CharField(max_length=255)
+    starter = models.BooleanField()
+    subtypes = models. """list of strings""" # arraylist postgres? serialize/deserialize json string?
+    supertypes = models. """list of strings""" # arraylist postgres? serialize/deserialize json string?
+    text = models.TextField(max_length=1000)
+    timeshifted = models.BooleanField()
+    toughness = models.CharField(max_length=10)
+    type = models.CharField(max_length=255)
+    types = models. """list of strings""" # arraylist postgres? serialize/deserialize json string?
+    variations = models. """list of strings""" # arraylist postgres? serialize/deserialize json string?
+    watermark = models.CharField(max_length=50)
 
 
 class CardPrice(models.Model):
