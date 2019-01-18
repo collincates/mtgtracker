40606dbe3138ba30c5cae5997fb38098eea38f86
collincates <collin.cates@gmail.com>
2019-01-18 13:47:02 -0800

    Card Detail View more

--- db/views.py
+++ db/views.py
@@ -4,8 +4,10 @@ from db.models import Card
 
 class CardListView(generic.ListView):
     model = Card
-    paginate_by = 50
+    paginate_by = 5000
 
 class CardDetailView(generic.DetailView):
     model = Card
-    
+
+
+[card.id for card in cards if thing in card.variations]
