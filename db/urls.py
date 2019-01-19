a646a21d85b73cb7d4be3c7a96fad01992637d99
collincates <collin.cates@gmail.com>
2019-01-18 23:50:04 -0800

    Adding page for cards by set

--- db/urls.py
+++ db/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('cards/', views.CardListView.as_view(), name='cards'),
+    path('cards/', views.CardListView.as_view(), name='card_list'),
+    path('cards/<slug:slug>', views.SetListView.as_view(), name='set_list'),
     path('card/<int:pk>/', views.CardDetailView.as_view(), name='card_detail'),
 ]
--- db/views.py
+++ db/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from db.models import Card
+from django.utils.text import slugify
 
 class CardListView(generic.ListView):
     model = Card
@@ -9,7 +10,16 @@ class CardListView(generic.ListView):
     # def get_queryset(self):
     #     return Card.objects.filter(set='FEM')
 
+class SetListView(generic.ListView):
+    model = Card
+    slug_field = 'set_name'
+    slug_url_kwarg = 'set_name'
+
+    # def get_queryset(self, set_name):
+    #     return Card.objects.filter(set_name=set_name)
+
+
 class CardDetailView(generic.DetailView):
     model = Card
-    pk_field = 'id'
+    # pk_field = 'id'
     # pk_url_kwarg = 'id'
