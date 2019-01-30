174a0ca8447ea9620118467f6c8eac7a43f66e7e
collincates <collin.cates@gmail.com>
2019-01-29 23:40:34 -0800

    Added add remove buttons to card_list

--- db/urls.py
+++ db/urls.py
@@ -5,13 +5,13 @@ from . import views
     # path('', )
     # path('', views.BrowseDatabaseView, name='browse-database'),
 urlpatterns = [
+    path('cards/', views.CardListView.as_view(), name='card_list'),
     re_path(r'^add/(?P<card_id>\d+)/$', views.add_card_to_collection, name='collection_add'),
     re_path(r'^remove/(?P<card_id>\d+)/$', views.remove_card_from_collection, name='collection_remove'),
     re_path(r'^collection/(?P<user_name>[-\w]+)/(?P<collection_name>[-\w]+)/$', views.collection_view, name='collection_view'),
     re_path(r'^card/(?P<card_slug>[-\w]+)$', views.CardDetailView.as_view(), name='card_detail'),
 ]
 
-    # path('cards/', views.CardListView.as_view(), name='card_list'),
     # path('<set_name>/', views.SetListView.as_view(), name='set_list'),
     # path('<user_name>/<collection_name>/', views.collection_view, name='collection_detail'),
     # path('<user_name>/<collection_name>/', views.CollectionDetailView.as_view(), name='collection_detail'),
