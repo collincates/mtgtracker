34612de7ec51ffdfba23d15c3fa686d86bdbc9e7
collincates <collin.cates@gmail.com>
2019-01-30 13:57:46 -0800

    Starting a collection_create view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.views.generic.base import TemplateView
 
+from accounts.models import User
 
 class HomeView(TemplateView):
     template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        return context
