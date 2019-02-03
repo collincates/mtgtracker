6b721df887a371e5553fa230e7b971e6d310f574
collincates <collin.cates@gmail.com>
2019-02-03 14:15:13 -0800

    DeckModelAdmin list_display

--- collection/admin.py
+++ collection/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from collection.models import Collection
 
+class DecksInline(admin.TabularInline):
+    model = Collection.decks.through
 
 @admin.register(Collection)
 class CollectionModelAdmin(admin.ModelAdmin):
+    inlines = [DecksInline]
+    exclude = ('decks',)
+
     list_display = ('name', 'owner',)
--- deck/admin.py
+++ deck/admin.py
@@ -3,21 +3,29 @@ from django.contrib import admin
 from deck.models import Deck
 from db.models import Card
 
-class DeckInline(admin.TabularInline):
+class CardsInline(admin.TabularInline):
     model = Deck.cards.through
-
+    extra = 1
 
 @admin.register(Deck)
 class DeckModelAdmin(admin.ModelAdmin):
-    inlines = [DeckInline]
+    inlines = [CardsInline]
+    exclude = ('cards',)
 
-    list_display = ('name', 'description')
+    list_display = ('name', 'description', 'owner')
+
+    def get_queryset(self, request):
+        qs = super(DeckModelAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(collection__owner=request.user)
 
     # def get_readonly_fields(self, request, obj=None):
     #     return [f.name for f in self.model._meta.fields]
 
-    def show_cards(self, obj):
-        return '\n'.join([card.name for card in obj.cards.all()])
-
-    def deck_cards(self, obj):
-        return "\n".join([card.name for card in obj.cards.all()])
+    def owner(self, deck):
+        return deck.collections.filter(
+            owner__username='ccc').first().owner.username
+    #
+    # def deck_cards(self, obj):
+    #     return "\n".join([card.name for card in obj.cards.all()])
